1e04e2ed241cf95cb715a28024142519f029fb2b
hendychua <hendychua@gmail.com>
2014-03-09 00:27:36 +0800

    duplicated main file to follow assignment submission requirements

--- src/SSL_Infinite.py
+++ src/SSL_Infinite.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     handler = StreamHandler()
     handler.setFormatter(logging.Formatter("(%(levelname)s) %(name)s: %(msg)s"))
 
-    root.setLevel(logging.DEBUG)
+    root.setLevel(logging.ERROR)
     root.addHandler(handler)
     
     # -1 for infinite amount
